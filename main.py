a574194976176a288595f61367f2e46bcdf86c15
georgesmbock <georgesmbo89@gmail.com>
2024-03-12 12:01:54 +0100

    mod 3

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import numpy as np
 import requests as rq
 from bs4 import BeautifulSoup as bs
 print(f"{9 + 9 + 2}")
-print("Let\'s go")
+print("Let\'s go!")
 print("Hello, World!")
 print("voir et merci")
