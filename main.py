f5cabcfac2fdbcf14a2f2c120f8aca090f653f97
Mohamed Tine <81871519+Dar-rius@users.noreply.github.com>
2024-03-12 16:09:45 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import requests as rq
 from bs4 import BeautifulSoup as bs
+# we test this program
 print(f"{9 + 9 + 2}")
 print("Let\'s go!")
 print("Hello, World!")
